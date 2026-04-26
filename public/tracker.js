dfba8110ef4aa869492bb7c7e05981a563ad0528
prayagfb45 <prayagfb45@gmail.com>
2026-04-27 01:33:26 +0530

    fix recharts dependency

--- public/tracker.js
+++ public/tracker.js
@@ -1,19 +1,17 @@
 const device = navigator.userAgent;
 
-fetch("https://ipapi.co/json/")
-  .then(res => res.json())
-  .then(ip => {
-    fetch("https://analytics-e5ih.onrender.com/api/track", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        site_id: siteId,
-        url: window.location.href,
-        referrer: document.referrer || "direct",
-        country: ip.country_name,
-        device: device
-      })
-    });
-  });
+(function () {
+  const site_id = "test123"; // ✅ DEFINE IT HERE
+
+  fetch("https://analytics-e5ih.onrender.com/api/track", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      site_id: site_id,
+      url: window.location.href,
+      referrer: document.referrer || "direct",
+    }),
+  }).catch(err => console.log("Tracking failed:", err));
+})();
