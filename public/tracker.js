020253258ce0be41bbc0cf73a06ea78a75fab106
prayagfb45 <prayagfb45@gmail.com>
2026-04-27 00:40:05 +0530

    move tracker to public

--- public/tracker.js
+++ public/tracker.js
@@ -2,18 +2,15 @@
   const script = document.currentScript;
   const siteId = script.getAttribute("data-site-id");
 
-  if (!siteId) return;
-
-  fetch("https://analytics.xyra.media/api/track", {
+  fetch("https://analytics-e5ih.onrender.com/api/track", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       site_id: siteId,
       url: window.location.href,
-      referrer: document.referrer,
-      timestamp: Date.now()
-    })
+      referrer: document.referrer || "direct",
+    }),
   });
 })();
